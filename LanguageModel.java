3856f5900c378a3b8611fec9cdc7192b384d8fa3
TomAvrahami1 <tom.avrahami@runi.ac.il>
2024-03-14 09:36:48 +0200

    test

--- LanguageModel.java
+++ LanguageModel.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class LanguageModel {
 
-    // The map of this model.
+    // The map of this model
     // Maps windows to lists of charachter data objects.
     HashMap<String, List> CharDataMap;
     
